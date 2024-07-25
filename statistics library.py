03888cfd567c3038cb8b4cb0add19752f80a86af
David Manresa <159567866+D1533@users.noreply.github.com>
2024-07-26 00:13:43 +0200

    Add files via upload

--- statistics library.py
+++ statistics library.py
@@ -1,213 +1,7 @@
 import math
 
-########################################################################################################################################
-
-# Common Probability distributions
-
-#######################################################################################################################################
-
-# Probability density functions
-def normal_pdf(x):
-    return (2*math.pi)**(-0.5)*math.exp(-0.5*(x**2))
-
-def t_student_pdf(nu, x):
-    return math.gamma(0.5 * (nu + 1.0)) / math.gamma(0.5 * nu) / (nu * math.pi)**0.5 * (1 + x**2/nu)**(-0.5*(nu + 1))
-
-def chi_squared_pdf(n, x):
-    return 1/(2**(n/2)*math.gamma(n/2))*x**(n/2-1)*math.exp(-x/2)
-
-def F_snedecor_pdf(n1,n2,x):
-    return (((n1*x)**n1*n2**n2)/(n1*x+n2)**(n1+n2))**0.5/(x*math.gamma(n1/2)*math.gamma(n2/2)/math.gamma((n1+n2)/2))
-def beta_pdf(a,b,x):
-    return x**(a-1)*(1-x)**(b-1)/(math.gamma(a)*math.gamma(b)/math.gamma(a+b))
-    
-# Probability mass functions
-def binomial_pmf(n, p, x):
-    return math.comb(n,x)*p**x*(1-p)**(n-x)
-
-def poisson_pmf(l, x):
-    return math.exp(-l)*l**x/math.factorial(x)
-
-###########################################################################################################################################
-
-# Abscissas
-
-###########################################################################################################################################
-# Returns the abscissa z_{alpha} such that P(Z > z_{alpha}) = alpha
-def normal_abscissa(alpha):
-    # Aproximates the integral of the pdf, and stops when the condition is satisfied
-    a = 0
-    b = 0.0001
-    dx = (b-a)
-    probability = 1
-    integral = 0
-    
-    while probability > alpha:
-        f_a = normal_pdf(a)
-        f_b = normal_pdf(b)
-        integral += f_b*dx + (f_a - f_b)*dx/2
-        probability = 1 - (0.5 + integral)
-        a = b
-        b += 0.0001
-    
-    return b
-
-# Returns the abscissa t_{nu,alpha} such that P(T > t_{nu,alpha}) = alpha
-def t_student_abscissa(nu, alpha):
-    a = 0
-    b = 0.0001
-    dx = (b-a)
-    probability = 1
-    integral = 0
-    while probability > alpha:
-        f_a = t_student_pdf(nu,a)
-        f_b = t_student_pdf(nu,b)
-        integral += f_b*dx + (f_a - f_b)*dx/2
-        probability = 1 - (0.5 + integral)
-        a = b
-        b += 0.0001
-        
-    return b
-
-# Returns the abscissa x_{n,alpha} such that P(X > x_{n,alpha}) = alpha
-def chi_squared_abscissa(n, alpha):
-    a = 0
-    b = 0.0001
-    dx = (b-a)
-    probability = 1
-    integral = 0
-    while probability > alpha:
-        f_a = chi_squared_pdf(n,a)
-        f_b = chi_squared_pdf(n,b)
-        integral += f_b*dx + (f_a - f_b)*dx/2
-        probability = 1 -  integral
-        a = b
-        b += 0.0001
-    
-    return b
-
-# Returns the abscissa x_{n1,n2,alpha} such that P(X > x_{n1,n2,alpha} = alpha)
-def beta_abscissa(n1,n2,alpha):
-    a = 0
-    b = 0.00005
-    dx = (b-a)
-    probability = 1
-    integral = 0
-    while probability > alpha:
-        f_a = beta_pdf(n1,n2,a)
-        f_b = beta_pdf(n1,n2,b)
-        integral += f_b*dx + (f_a - f_b)*dx/2
-        probability = 1 -  integral
-        a = b
-        b += 0.00005
-    
-    return b
-
-def f_snedecor_abscissa(n1,n2, alpha):
-    x = beta_abscissa(n1/2,n2/2,alpha)
-    return n2*x/(n1*(1-x))
-
-
-
-#####################################################################################################################################
-
-# Confidence intervals
-
-####################################################################################################################################
-
-# Returns the interval [a,b] such that the probability of the mean of the data set X be in [a,b] is (1 - alpha)
-def normal_mean_confidence_interval(X,  alpha, standard_deviation = None):
-    # Computes the mean of the data set X
-    mean = 0
-    n = len(X)
-    for i in range(n):
-        mean += X[i]/n
-   
-    # The user supose that the data follows a Normal distribution
-    if standard_deviation is None:
-        # Computes the sample standard deviation
-        V = 0
-        for i in range(len(X)):
-            V += (X[i] - mean)**2
-        S = (V / (n-1))**(0.5)
-        # Interval
-        a = mean - t_student_abscissa(len(X)-1,alpha/2)*S/n**(0.5)
-        b = mean + t_student_abscissa(len(X)-1,alpha/2)*S/n**(0.5)
-    # The user doesn't supose that the data follows a Normal distribution (this works better for large samples with n > 30)
-    else:
-        # Interval endpoints
-        a = round(mean - normal_abscissa(alpha/2)*standard_deviation/n**(0.5), 4)
-        b = round(mean + normal_abscissa(alpha/2)*standard_deviation/n**(0.5), 4)
-
-    return [a, b]
-
-def normal_mean_difference_confidence_interval(X, Y, alpha):
-    x = mean(X)
-    y = mean(Y)
-    s_x = variance(X)
-    s_y = variance(Y)
-    n_x = len(X)
-    n_y = len(Y)
-
-    S_p = ( ((n_x - 1)*s_x + (n_y - 1)*s_y)/(n_x + n_y - 2) )**0.5
-    if n_x >= 30 and n_y >= 30:
-        a = (x - y) - normal_abscissa(alpha/2)*S_p*(1/n_x + 1/n_y)**0.5
-        b = (x - y) + normal_abscissa(alpha/2)*S_p*(1/n_x + 1/n_y)**0.5
-    else:
-        a = (x - y) - t_student_abscissa(n_x + n_y - 2, alpha/2)*S_p*(1/n_x + 1/n_y)**0.5
-        b = (x - y) + t_student_abscissa(n_x + n_y - 2, alpha/2)*S_p*(1/n_x + 1/n_y)**0.5
-    
-    return [a,b]
-
-def binomial_proportion_confidence_interval(X, alpha):
-    # X is a vector of 1's and 0's where 1 means the element satisfy the property studied (Bernoulli)
-    n = len(X)
-    p = sum(X)/n
-
-    a = (p - normal_abscissa(alpha/2)/n**0.5*(p*(1-p))**0.5)
-    b = (p + normal_abscissa(alpha/2)/n**0.5*(p*(1-p))**0.5)
-
-    return [a, b]
-
-def poisson_mean_confidence_interval(X, alpha):
-    # Computes the mean of the data set X
-    mean = 0
-    n = len(X)
-    for i in range(n):
-        mean += X[i]/n
-
-    # Interval endpoints
-    a = round(mean - normal_abscissa(alpha/2)*(mean/n)**0.5, 4)
-    b = round(mean - normal_abscissa(alpha/2)*(mean/n)**0.5, 4)
-
-    return [a,b]
-
-def normal_variance_confidence_interval(X, alpha, mean=None):
-    n = len(X)
-    if mean is None:
-        mean = 0
-        for i in range(n):
-            mean += X[i]/n
-        variance_sample = 0
-        for i in range(n):
-            variance_sample += (X[i] - mean)**2/(n-1)
-        a = (n-1)*variance_sample/(chi_squared_abscissa(n-1, alpha/2))
-        b = (n-1)*variance_sample/(chi_squared_abscissa(n-1, 1-  alpha/2))
-
-        return [a, b]
-    k = 0
-    for i in range(n):
-        k += (X[i] - mean)**2
-    
-    a = round(k / chi_squared_abscissa(n,alpha/2), 4)
-    b = round(k / chi_squared_abscissa(n, 1 - alpha/2), 4)
-
-    return [a, b]
-
 ######################################################################################
-
 # Descriptive statistics
-
 ######################################################################################
 
 def mean(X):
@@ -228,13 +22,13 @@ def central_moment(X,k):
     mu = mean(X)
     return sum([(x - mu)**k for x in X])/len(X)
 
+def percentile(X, k):
+    N = len(X)
+    n = math.ceil(k/100*(N-1))
+    return sorted(X)[n]
+
 def median(X):
-    X_sorted = sorted(X)
-    n = len(X)
-    if n % 2 == 1:
-        return X_sorted[n // 2]
-    else:
-        return (X_sorted[n // 2 - 1] + X_sorted[n // 2 + 1])/2
+    return percentile(X, 50)
 
 def mode(X):
     count_dict = {}
@@ -264,7 +58,9 @@ def descriptive_statistics_summary(X):
         'Coefficient of Variation': coefficient_of_variation(X),
         'Skewness' : central_moment(X,3),
         'Kurtosis' : central_moment(X,4),
+        '0.25 Percentile' : percentile(X, 25),
         'Median' : median(X),
+        '0.75 Percentile' : percentile(X,75),
         'Mode' : mode(X)
     }
 
@@ -277,3 +73,199 @@ def covariance(X,Y):
 
 def pearson_coeff(X,Y):
     return covariance(X,Y)/( variance(X)**0.5*variance(Y)**0.5 )
+
+
+##################################################################################################
+# Common Probability distributions
+##################################################################################################
+
+# Probability density functions
+def normal_pdf(x):
+    return (2*math.pi)**(-0.5)*math.exp(-0.5*(x**2))
+
+def t_student_pdf(nu, x):
+    return math.gamma(0.5 * (nu + 1.0)) / math.gamma(0.5 * nu) / (nu * math.pi)**0.5 * (1 + x**2/nu)**(-0.5*(nu + 1))
+
+def chi_squared_pdf(n, x):
+    return 1/(2**(n/2)*math.gamma(n/2))*x**(n/2-1)*math.exp(-x/2)
+
+def F_snedecor_pdf(n1,n2,x):
+    return (((n1*x)**n1*n2**n2)/(n1*x+n2)**(n1+n2))**0.5/(x*math.gamma(n1/2)*math.gamma(n2/2)/math.gamma((n1+n2)/2))
+def beta_pdf(a,b,x):
+    return x**(a-1)*(1-x)**(b-1)/(math.gamma(a)*math.gamma(b)/math.gamma(a+b))
+    
+# Probability mass functions
+def binomial_pmf(n, p, x):
+    return math.comb(n,x)*p**x*(1-p)**(n-x)
+
+def poisson_pmf(l, x):
+    return math.exp(-l)*l**x/math.factorial(x)
+
+#######################################################################################################
+# Abscissas
+#######################################################################################################
+
+# Returns the abscissa z_{alpha} such that P(Z > z_{alpha}) = alpha
+def normal_abscissa(alpha):
+    # Aproximates the integral of the pdf, and stops when the condition is satisfied
+    a = 0
+    b = 0.0001
+    dx = (b-a)
+    probability = 1
+    integral = 0
+    
+    while probability > alpha:
+        f_a = normal_pdf(a)
+        f_b = normal_pdf(b)
+        integral += f_b*dx + (f_a - f_b)*dx/2
+        probability = 1 - (0.5 + integral)
+        a = b
+        b += dx
+    
+    return b
+
+# Returns the abscissa t_{nu,alpha} such that P(T > t_{nu,alpha}) = alpha
+def t_student_abscissa(nu, alpha):
+    a = 0
+    b = 0.0001
+    dx = (b-a)
+    probability = 1
+    integral = 0
+    while probability > alpha:
+        f_a = t_student_pdf(nu,a)
+        f_b = t_student_pdf(nu,b)
+        integral += f_b*dx + (f_a - f_b)*dx/2
+        probability = 1 - (0.5 + integral)
+        a = b
+        b += dx
+        
+    return b
+
+# Returns the abscissa x_{n,alpha} such that P(X > x_{n,alpha}) = alpha
+def chi_squared_abscissa(n, alpha):
+    a = 0
+    b = 0.0001
+    dx = (b-a)
+    probability = 1
+    integral = 0
+    while probability > alpha:
+        f_a = chi_squared_pdf(n,a)
+        f_b = chi_squared_pdf(n,b)
+        integral += f_b*dx + (f_a - f_b)*dx/2
+        probability = 1 -  integral
+        a = b
+        b += dx
+    
+    return b
+
+# Returns the abscissa x_{n1,n2,alpha} such that P(X > x_{n1,n2,alpha} = alpha)
+def beta_abscissa(n1,n2,alpha):
+    a = 0
+    b = 0.00005
+    dx = (b-a)
+    probability = 1
+    integral = 0
+    while probability > alpha:
+        f_a = beta_pdf(n1,n2,a)
+        f_b = beta_pdf(n1,n2,b)
+        integral += f_b*dx + (f_a - f_b)*dx/2
+        probability = 1 -  integral
+        a = b
+        b += dx
+    
+    return b
+
+def f_snedecor_abscissa(n1,n2, alpha):
+    x = beta_abscissa(n1/2,n2/2,alpha)
+    return n2*x/(n1*(1-x))
+
+
+
+###################################################################################################
+# Confidence intervals
+###################################################################################################
+
+# Returns the interval [a,b] such that the probability of the mean of the data set X be in [a,b] is (1 - alpha)
+def normal_mean_confidence_interval(X,  alpha, standard_deviation = None):
+    # Computes the mean of the data set X
+    mean = mean(X)
+
+    # The user supose that the data follows a Normal distribution
+    if standard_deviation is None:
+        # Computes the sample standard deviation
+        S = sample_variance(X)
+        # Interval
+        t = t_student_abscissa(len(X)-1,alpha/2)
+        a = mean - t*S/n**(0.5)
+        b = mean + t*S/n**(0.5)
+    # The user doesn't supose that the data follows a Normal distribution (this works better for large samples with n > 30)
+    else:
+        # Interval endpoints
+        z = normal_abscissa(alpha/2)
+        a = mean - z*standard_deviation/n**(0.5)
+        b = mean + z*standard_deviation/n**(0.5)
+
+    return [a, b]
+
+def normal_mean_difference_confidence_interval(X, Y, alpha):
+    x = mean(X)
+    y = mean(Y)
+    s_x = variance(X)
+    s_y = variance(Y)
+    n_x = len(X)
+    n_y = len(Y)
+
+    S_p = ( ((n_x - 1)*s_x + (n_y - 1)*s_y)/(n_x + n_y - 2) )**0.5
+    if n_x >= 30 and n_y >= 30:
+        z = normal_abscissa(alpha/2)
+        a = (x - y) - z*S_p*(1/n_x + 1/n_y)**0.5
+        b = (x - y) + z*S_p*(1/n_x + 1/n_y)**0.5
+    else:
+        t = t_student_abscissa(n_x + n_y - 2, alpha/2)
+        a = (x - y) - t*S_p*(1/n_x + 1/n_y)**0.5
+        b = (x - y) + t*S_p*(1/n_x + 1/n_y)**0.5
+    
+    return [a,b]
+
+def binomial_proportion_confidence_interval(X, alpha):
+    # X is a vector of 1's and 0's where 1 means the element satisfy the property studied (Bernoulli)
+    n = len(X)
+    p = sum(X)/n
+    
+    z = normal_abscissa(alpha/2)
+    a = (p - z/n**0.5*(p*(1-p))**0.5)
+    b = (p + z/n**0.5*(p*(1-p))**0.5)
+
+    return [a, b]
+
+def poisson_mean_confidence_interval(X, alpha):
+    mean = mean(X)
+
+    # Interval endpoints
+    z = normal_abscissa(alpha/2)
+    a = mean - z*(mean/n)**0.5
+    b = mean - z*(mean/n)**0.5
+
+    return [a,b]
+
+def normal_variance_confidence_interval(X, alpha, mean=None):
+    n = len(X)
+    if mean is None:
+        mean = mean(X)
+        for i in range(n):
+            mean += X[i]/n
+        S = sample_variance(X)
+    
+        a = (n-1)*S/(chi_squared_abscissa(n-1, alpha/2))
+        b = (n-1)*S/(chi_squared_abscissa(n-1, 1-  alpha/2))
+
+        return [a, b]
+    k = 0
+    for i in range(n):
+        k += (X[i] - mean)**2
+    
+    a = k / chi_squared_abscissa(n,alpha/2)
+    b = k / chi_squared_abscissa(n, 1 - alpha/2)
+
+    return [a, b]
+
